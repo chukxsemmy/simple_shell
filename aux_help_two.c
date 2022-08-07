2b04b5a2e9855b994b0b546a2791473c4fe30b8b
Chukxsemmy <chukxsemmy@gmail.com>
2022-08-08 05:52:52 +0800

    betty error fixed

--- aux_help_two.c
+++ aux_help_two.c
@@ -51,12 +51,11 @@ void aux_help_cd(void)
 
 void aux_help_exit(void)
 {
-        char *help = "exit: exit [n]\n Exit shell.\n";
+	char *help = "exit: exit [n]\n Exit shell.\n";
 
-        write(STDOUT_FILENO, help, _strlen(help));
+	write(STDOUT_FILENO, help, _strlen(help));
         help = "Exits the shell with a status of N. If N is ommited, the exit";
         write(STDOUT_FILENO, help, _strlen(help));
         help = "statusis that of the last command executed\n";
         write(STDOUT_FILENO, help, _strlen(help));
 }
-
