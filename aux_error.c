ec05c9a7934d7dbf01cea52af1836e77e69b395b
Chukxsemmy <chukxsemmy@gmail.com>
2022-08-08 05:25:00 +0800

    more than 5 functions in aux error file

--- aux_error.c
+++ aux_error.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcat_cd - function that concatenates 
+ * strcat_cd - function that concatenates
  *             the message for cd error
  * @datash: data relevant (directory)
  * @msg: message
@@ -40,7 +40,7 @@ char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 
 /**
  * error_get_cd - error message for change directory
- * 		  command in get_cd
+ *                command in get_cd
  * @datash: data relevant (directory)
  * Return: error message
  */
@@ -184,36 +184,3 @@ char *error_env(data_shell *datash)
 	return (error);
 }
 
-/**
- * error_path_126 - error message for path and failure denied permission.
- * @datash: data relevant (counter, arguments).
- *
- * Return: The error string.
- */
-
-char *error_path_126(data_shell *datash)
-{
-	int len;
-	char *ver_str;
-	char *error;
-
-	ver_str = aux_itoa(datash->counter);
-	len = _strlen(datash->av[0]) + _strlen(ver_str);
-	len += _strlen(datash->args[0]) + 24;
-	error = malloc(sizeof(char) * (len + 1));
-	if (error == 0)
-	{
-		free(error);
-		free(ver_str);
-		return (NULL);
-	}
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, ": Permission denied\n");
-	_strcat(error, "\0");
-	free(ver_str);
-	return (error);
-}
