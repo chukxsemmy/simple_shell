4fbde90535fdd074455bfcc0c4e4caf2b552f9bc
Chukxsemmy <chukxsemmy@gmail.com>
2022-08-10 20:45:16 +0800

    betty fix

--- shell_loop.c
+++ shell_loop.c
@@ -21,7 +21,7 @@ char *without_comment(char *in)
 				return (NULL);
 			}
 
-			if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+			if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ' ')
 				up_to = i;
 		}
 	}
@@ -49,7 +49,7 @@ void shell_loop(data_shell *datash)
 	loop = 1;
 	while (loop == 1)
 	{
-		write(STDIN_FILENO, "$ ", 4);
+		write(STDIN_FILENO, "$", 4);
 		input = read_line(&i_eof);
 		if (i_eof != -1)
 		{
